df08b4e161f667fca33bc4d2f8451ef08ba19ced
MatheusKiyota <matheuslk2911@gmail.com>
2025-02-05 09:11:58 -0300

    atualização do trienoController

--- controladores/treinoController.js
+++ controladores/treinoController.js
@@ -1,27 +1,46 @@
 const database = require('../database/database');
 
-
-exports.listarTreinos = (req, res) => res.json(database.select('treinos'));
+exports.listarTreinos = (req, res) => {
+    if (database.select('treinos')) {
+        res.json(database.select('treinos'));
+    } else {
+        res.status(404).json({ message: 'Nenhum treino encontrado' });
+    }
+};
 
 exports.criarTreino = (req, res) => {
     const { nome, duracao } = req.body;
-    if (!nome || !duracao) return res.status(400).json({ message: 'Nome e duração obrigatórios!' });
-
-    database.insert('treinos', nome, { nome, duracao });
-    res.status(201).json({ message: `Treino de ${nome} criado!` });
+    if (!nome || !duracao) {
+        res.status(400).json({ message: 'Nome e duração obriagtorios' });
+    } else {
+        database.insert('treinos', nome, { nome, duracao });
+        res.status(201).json({ message: `Treino de ${nome} criado!` });
+    }
 };
 
 exports.buscarTreino = (req, res) => {
     const treino = database.select('treinos')[req.params.nome];
-    treino ? res.json(treino) : res.status(404).json({ message: 'Treino não encontrado' });
+    if (treino) {
+        res.json(treino);
+    } else {
+        res.status(404).json({ message: 'Treino não encontrado' });
+    }
 };
 
 exports.atualizarTreino = (req, res) => {
     const atualizado = database.update('treinos', req.params.nome, req.body);
-    atualizado ? res.json({ message: `Treino de ${req.params.nome} atualizado!` }) : res.status(404).json({ message: 'Treino não encontrado' });
+    if (atualizado) {
+        res.json({ message: `Treino de ${req.params.nome} atualizado!` });
+    } else {
+        res.status(404).json({ message: 'Treino não encontrado' });
+    }
 };
 
 exports.excluirTreino = (req, res) => {
     const deletado = database.delete('treinos', req.params.nome);
-    deletado ? res.json({ message: `Treino de ${req.params.nome} excluído!` }) : res.status(404).json({ message: 'Treino não encontrado' });
+    if (deletado) {
+        res.json({ message: `Treino de ${req.params.nome} excluído!` });
+    } else {
+        res.status(404).json({ message: 'Treino não encontrado' });
+    }
 };
